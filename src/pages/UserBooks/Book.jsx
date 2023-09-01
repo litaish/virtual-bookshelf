07760bb84bbf3df457815bdc3e3d1958f30ff810
Lita Strode <l.strode000@gmail.com>
2023-09-01 18:10:16 +0300

    Add fake book api

--- src/pages/UserBooks/Book.jsx
+++ src/pages/UserBooks/Book.jsx
@@ -6,15 +6,17 @@ import Icon from '@mdi/react';
 import { mdiCheckCircle } from '@mdi/js';
 import styles from './UserBooks.module.css';
 
-export const Book = ({ imgSrc, id, title, author, rating, isRead }) => {
+export const Book = ({ imgSrc, id, title, authors, rating, isRead }) => {
   return (
     <div className='relative'>
       <div className='flex flex-col justify-center items-center gap-1 text-center'>
-        <Link to='/book/:id'>
-          <img className='rounded-md mb-2' src={imgSrc} alt={`${author} - ${title} `} />
+        <Link to={`/book/${id}`}>
+          <img className='rounded-md mb-2' src={imgSrc} alt={`${title} `} />
         </Link>
         <p className='text-gray-800 text-xl font-medium'>{title}</p>
-        <p className='text-xl text-gray-600'>{author}</p>
+        {authors.map((author) => {
+          return <p key={author.name} className='text-xl text-gray-600'>{author.name}</p>;
+        })}
         <Rating
           value={rating}
           readOnly={true}
@@ -34,7 +36,7 @@ Book.propTypes = {
   id: PropTypes.string.isRequired,
   imgSrc: PropTypes.string,
   title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
+  authors: PropTypes.array.isRequired,
   rating: PropTypes.number.isRequired,
   isRead: PropTypes.bool.isRequired,
 };
--- src/pages/UserBooks/UserBooksView.jsx
+++ src/pages/UserBooks/UserBooksView.jsx
@@ -4,38 +4,36 @@ import { Book } from "./Book";
 import Icon from '@mdi/react';
 import { mdiPlus } from '@mdi/js';
 import styles from './UserBooks.module.css';
+import { useEffect, useState } from "react";
 
 export const UserBooksView = () => {
+  const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+      let requestOptions = {
+          method: "GET",
+          redirect: "follow",
+      }
+      // Fake JSON API
+      fetch('http://localhost:3030/books', requestOptions)
+          .then((response) => response.json())
+          .then((result) => setBooks(result))
+          .catch((error) => console.log("error", error));
+  }
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
   return (
     <main className="p-8">
       <PrimaryButton text='Add new book' icon={<Icon path={mdiPlus} size={1.5} />} />
       <Header text='My Books' bookCount={100} className='mt-4' />
       <div>SEARCH BAR HERE</div>
       <div className={`${styles.grid_books} mt-8`}>
-      <Book 
-        id='1' 
-        imgSrc='https://books.google.lv/books/publisher/content?id=vnbgDwAAQBAJ&hl=lv&pg=PA4&img=1&zoom=3&bul=1&sig=ACfU3U1lRTcuSZw36QN296qN1nN4VvWxdg&w=1280'
-        title='Harry Potter: Exploring Hogwarts'
-        author='Jody Revenson'
-        rating={4}
-        isRead={false}
-         />
-                 <Book 
-        id='1' 
-        imgSrc='https://books.google.lv/books/publisher/content?id=vnbgDwAAQBAJ&hl=lv&pg=PA4&img=1&zoom=3&bul=1&sig=ACfU3U1lRTcuSZw36QN296qN1nN4VvWxdg&w=1280'
-        title='Harry Potter: Exploring Hogwarts'
-        author='Jody Revenson'
-        rating={4}
-        isRead={true}
-         />
-                 <Book 
-        id='1' 
-        imgSrc='https://books.google.lv/books/publisher/content?id=vnbgDwAAQBAJ&hl=lv&pg=PA4&img=1&zoom=3&bul=1&sig=ACfU3U1lRTcuSZw36QN296qN1nN4VvWxdg&w=1280'
-        title='Harry Potter: Exploring Hogwarts'
-        author='Jody Revenson'
-        rating={4}
-        isRead={false}
-         />
+         {books.map((book) => {
+          return <Book key={book.id} id={book.id} title={book.title} authors={book.authors} rating={book.rating} isRead={book.isRead} imgSrc={book.imgSrc}/>
+         })}
       </div>
     </main>
   );
