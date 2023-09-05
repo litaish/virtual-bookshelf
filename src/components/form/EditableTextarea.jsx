673cff89a5b2ffe651151336b573202f51dde180
Lita Strode <l.strode000@gmail.com>
2023-09-05 18:20:58 +0300

    Change editable textarea to not be resizable

--- src/components/form/EditableTextarea.jsx
+++ src/components/form/EditableTextarea.jsx
@@ -25,7 +25,7 @@ export const EditableTextarea = ({ label, id, placeholder, name, initialValue })
         <textarea
           value={value}
           onChange={handleChange}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg block w-full p-2.5 md:text-2xl"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg block w-full p-2.5 md:text-2xl resize-none"
           name={name}
           disabled={isEditable}
           id={id}
