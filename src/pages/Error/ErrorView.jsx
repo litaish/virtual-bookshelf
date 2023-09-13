7795cb546da0bd9154afc4cd7ac2b9412db5a90f
litaish <l.strode000@gmail.com>
2023-09-13 11:20:07 +0300

    Style error view component

--- src/pages/Error/ErrorView.jsx
+++ src/pages/Error/ErrorView.jsx
@@ -1,15 +1,19 @@
 import { useRouteError } from 'react-router-dom';
+import { Layout } from '../../components/index';
 
 export const ErrorView = () => {
   const error = useRouteError();
 
   return (
-    <div>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div className='min-h-screen font-inter flex flex-col'>
+      <Layout.Navbar />
+      <main className='flex flex-grow flex-col justify-center items-center gap-4'>
+        <h1 className='text-3xl font-semibold'>Oops!</h1>
+        <p className='text-2xl'>Sorry, an unexpected error has occurred.</p>
+        <p className='text-xl'>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </main>
     </div>
   );
 };
